1f42c0cfe2a4839f537cd80b5db33399e5e24870
Code520455 <130625646+Code520455@users.noreply.github.com>
2024-01-09 11:57:25 +1030

    Update component.py

--- component.py
+++ component.py
@@ -7,3 +7,19 @@
 # Email: 520456@learning.eynesbury.edu.au
 # Description: component class
 # This is my own work as defined by the Academic Integrity Policy
+
+from abc import ABC, abstractmethod
+
+class Component(ABC):
+    '''
+    Abstract base class representing a component.
+
+    Attributes:
+    - name (str): The name of the component.
+    - price (float): The price of the component in dollars and cents.
+
+    Author: Rahul Saha
+    '''
+
+    def __init__(self, name:str,) -> None:
+        super().__init__()
