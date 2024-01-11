82389008dccbfeaa0fa2eb12542561b569a4593a
Code520455 <130625646+Code520455@users.noreply.github.com>
2024-01-11 16:03:37 +1030

    Create light_globe.py

---
+++ light_globe.py
@@ -0,0 +1,9 @@
+# File: light_globe.py
+# Author: RAHUL SAHA
+# ID: 520455
+# Email: 520455@learning.eynesbury.edu.au
+# Author: Rajeswari Roy
+# ID: 520456
+# Email: 520456@learning.eynesbury.edu.au
+# Description: light_globe class
+# This is my own work as defined by the Academic Integrity Policy
