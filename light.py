54b2af7ba32f5c17b62d7074d156c38df41c0368
Code520455 <130625646+Code520455@users.noreply.github.com>
2024-01-11 18:22:20 +1030

    Update light.py

--- light.py
+++ light.py
@@ -7,3 +7,46 @@
 # Email: 520456@learning.eynesbury.edu.au
 # Description: component class
 # This is my own work as defined by the Academic Integrity Policy
+
+from multiprocessing import Value
+from output_component import OutputComponent
+from abc import ABC, abstractmethod
+
+class Light(OutputComponent, ABC):
+    '''
+    Abstract class representing a light. This class is a subclass of OutputComponent.
+    Attributes:
+    - name (str): The name of the light
+    - price (float): The price of the light in dollars and cents
+    - voltage (float): The voltage of the light in volts.
+    - current (float): The current of the light in milliamps.
+    - color (str): The color of the light.
+    Author: Rahul Saha
+    '''
+
+    def __init__(self, name: str, price:float, voltage:float, current_mA: float, color:str):
+        super().__init__(name, price, voltage, current_mA)
+        self._color = color
+
+    @property
+    def color(self) -> str:
+        '''Get the colour of the light''' 
+        self._color = Value
+
+    @abstractmethod
+    def duplicate(self) -> 'Light':
+        '''Create a duplicate copy of the light'''
+        pass
+
+    @abstractmethod
+    def parse_csv(self, csv_string: str) -> 'Light':
+        '''
+        Parse a CSV string to recreate a light object.
+
+        Parameters:
+        - csv_string (str): The CSV string containing light information.
+
+        Returns:
+        - Light: A new light object.
+        '''
+        pass
