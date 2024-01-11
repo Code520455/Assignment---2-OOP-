5b0a47cf40ff6ea60d412d119ac03063a51dfefc
Code520455 <130625646+Code520455@users.noreply.github.com>
2024-01-11 15:56:48 +1030

    Create file_handler.py

---
+++ file_handler.py
@@ -0,0 +1,9 @@
+# File: file_handler.py
+# Author: RAHUL SAHA
+# ID: 520455
+# Email: 520455@learning.eynesbury.edu.au
+# Author: Rajeswari Roy
+# ID: 520456
+# Email: 520456@learning.eynesbury.edu.au
+# Description: It will handle the other classes
+# This is my own work as defined by the Academic Integrity Policy
