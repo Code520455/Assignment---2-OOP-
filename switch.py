d8a2072e6cf6eb5701216b58159d31ba100066dc
Code520455 <130625646+Code520455@users.noreply.github.com>
2024-01-11 16:08:27 +1030

    Create switch.py

---
+++ switch.py
@@ -0,0 +1,9 @@
+# File: switch.py
+# Author: RAHUL SAHA
+# ID: 520455
+# Email: 520455@learning.eynesbury.edu.au
+# Author: Rajeswari Roy
+# ID: 520456
+# Email: 520456@learning.eynesbury.edu.au
+# Description: switch class
+# This is my own work as defined by the Academic Integrity Policy
