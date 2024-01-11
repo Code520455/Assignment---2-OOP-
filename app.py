dbc0e536e0d65def230253d821ac96807a5fc9b9
Code520455 <130625646+Code520455@users.noreply.github.com>
2024-01-11 16:01:14 +1030

    Necessary classes

---
+++ app.py
@@ -0,0 +1,9 @@
+# File: app.py
+# Author: RAHUL SAHA
+# ID: 520455
+# Email: 520455@learning.eynesbury.edu.au
+# Author: Rajeswari Roy
+# ID: 520456
+# Email: 520456@learning.eynesbury.edu.au
+# Description: This is the main application where user can do whatever they want. 
+# This is my own work as defined by the Academic Integrity Policy
---
+++ input_component.py
@@ -0,0 +1,9 @@
+# File: imput_component.py
+# Author: RAHUL SAHA
+# ID: 520455
+# Email: 520455@learning.eynesbury.edu.au
+# Author: Rajeswari Roy
+# ID: 520456
+# Email: 520456@learning.eynesbury.edu.au
+# Description: input_component class
+# This is my own work as defined by the Academic Integrity Policy
