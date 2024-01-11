9111a29d84523e0b535ea6f41bf3ec7680b648d7
Code520455 <130625646+Code520455@users.noreply.github.com>
2024-01-11 15:55:34 +1030

    Make the necessary classes

---
+++ power_supply.py
@@ -0,0 +1,9 @@
+# File: power_supply.py
+# Author: RAHUL SAHA
+# ID: 520455
+# Email: 520455@learning.eynesbury.edu.au
+# Author: Rajeswari Roy
+# ID: 520456
+# Email: 520456@learning.eynesbury.edu.au
+# Description: power supply class
+# This is my own work as defined by the Academic Integrity Policy
