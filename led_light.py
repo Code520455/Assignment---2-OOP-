43ff8683408bcfd8aceeae113795dfc86aa7e084
Code520455 <130625646+Code520455@users.noreply.github.com>
2024-01-11 16:02:51 +1030

    Create led_light.py

---
+++ led_light.py
@@ -0,0 +1,9 @@
+# File: led_light.py
+# Author: RAHUL SAHA
+# ID: 520455
+# Email: 520455@learning.eynesbury.edu.au
+# Author: Rajeswari Roy
+# ID: 520456
+# Email: 520456@learning.eynesbury.edu.au
+# Description: led_light class
+# This is my own work as defined by the Academic Integrity Policy
