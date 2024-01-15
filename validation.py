22367bdc7592d4412a0a3fedcc6e153478fcdef0
Code520455 <520455@eynesbury.sa.edu.au>
2024-01-15 22:08:36 +1030

    Update validation.py

--- validation.py
+++ validation.py
@@ -7,3 +7,18 @@
 # Email: 520456@learning.eynesbury.edu.au
 # Description: This file will help to validate all the data.
 # This is my own work as defined by the Academic Integrity Policy
+
+
+from typing import List
+
+def str_input(text: str, options: List[str] = []) -> str:
+    '''
+    Get a valid string input from the user.
+
+    Parameters:
+    - text (str): The prompt text for input.
+    - options (List[str]): List of valid options (case-insensitive).
+
+    Returns:
+    - str: The user input.
+    '''
