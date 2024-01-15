359e1dfff584765b6f952ef51e4588d4f735ca9a
Code520455 <130625580+RajeswariRoy1@users.noreply.github.com>
2024-01-15 19:12:13 +1030

    Update input_component.py

--- input_component.py
+++ input_component.py
@@ -7,3 +7,35 @@
 # Email: 520456@learning.eynesbury.edu.au
 # Description: input_component class
 # This is my own work as defined by the Academic Integrity Policy
+
+from component import Component
+from abc import ABC, abstractmethod
+
+class InputComponent(Component, ABC):
+    '''
+    Abstract class representing an input component. This class is a subclass of Component.
+
+    Attributes:
+    - name (str): The name of the input component.
+    - price (float): The price of the input component in dollar and cents.
+    - type (str): The type of the input component.
+    - voltage (float): The voltage of the input component in volts.
+
+    Author: Rajeswari Roy
+    '''
+
+    def __init__(self, name: str, price: float, input_type: str, voltage: float):
+        super().__init__(name, price)
+        self._input_type = input_type
+        self._voltage = voltage
+
+    @property
+    def input_type(self) -> str:
+        '''Get the type of the input component'''
+        return self._input_type
+    
+    @input_type.setter
+    def input_type(self, value: str) -> None:
+        '''Set the type of the input component'''
+        self._input_type = value
+        
