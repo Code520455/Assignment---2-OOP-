2b7ca29d7eec1d478255558024410ddfccd4fefe
Code520455 <130625580+RajeswariRoy1@users.noreply.github.com>
2024-01-15 21:01:03 +1030

    Update solar_panel.py

--- solar_panel.py
+++ solar_panel.py
@@ -7,3 +7,33 @@
 # Email: 520456@learning.eynesbury.edu.au
 # Description: solar_panel class
 # This is my own work as defined by the Academic Integrity Policy
+
+from power_supply import PowerSupply
+
+class SolarPanel(PowerSupply):
+    '''
+    Represents a solar panel power supply that works in sunlight.
+
+    Attributes:
+    - name (str): The name of the solar panel.
+    - voltage (float): The voltage of the solar panel in volts.
+    - current_mA (float): The current of the solar panel in milliamps.
+    - price (float): The price of the solar panel in dollars and cents.
+
+    Author: Rajeswari Roy
+    '''
+
+    def __init__(self, name: str, voltage: float, current_mA: float, price: float):
+        super().__init__(name, price, voltage)
+        self._current_mA = current_mA
+
+    @property
+    def current_mA(self) -> float:
+        '''Get the current of the solar panel.'''
+        return self._current_mA
+    
+    @current_mA.setter
+    def current_mA(self, value:float) -> None:
+        '''Set the current of the solar panel'''
+        self._current_mA = value
+        
