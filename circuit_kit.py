09ab01ed3b6d4a6ecf23e4e4eddf0a2c10ceabc1
Code520455 <520455@eynesbury.sa.edu.au>
2024-01-16 10:24:43 +1030

    Update circuit_kit.py

--- circuit_kit.py
+++ circuit_kit.py
@@ -7,3 +7,29 @@
 # Email: 520456@learning.eynesbury.edu.au
 # Description: light_kit class
 # This is my own work as defined by the Academic Integrity Policy
+
+from typing import Dict
+from component import Component
+from solar_panel import SolarPanel
+from battery import Battery
+
+class CircuitKit:
+    def __init__(self, name: str, count: int) :
+        self._name = name
+        self._count = count
+
+    @property
+    def name(self) -> str:
+        '''Get the name of the circuit kit.'''
+        return self._name
+    
+    @property
+    def count(self) -> int:
+        '''Get the quantity of the circuit kit.'''
+        return self._count
+    
+    @count.setter
+    def count(self, count: int) -> None:
+        '''Set the quantity of the circuit kit'''
+        self._count = count
+        
