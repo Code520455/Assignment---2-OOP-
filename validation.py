569007d103ed6fccc555be0585464a4838d5b369
Code520455 <520455@eynesbury.sa.edu.au>
2024-01-15 22:34:17 +1030

    Update validation.py

--- validation.py
+++ validation.py
@@ -30,3 +30,75 @@ def str_input(text: str, options: List[str] = []) -> str:
             continue
         return inp
     
+def int_input(text: str, start: int = 0, end: int = 100_000_000) -> int:
+    '''
+    Get a valid integer input from the user within a specified range.
+    
+    Parameters:
+    - text (str): The prompt text for input.
+    - start (int): The minimum allowed value.
+    - end (int): The maximum allowed value.
+
+    Returns:
+    - int: The user input.
+    '''
+    while True:
+        inp = input(text)
+
+        if is_int(inp) and start <= int(inp) <= end:
+            return int(inp)
+        print(f"Wrong input, must be a number between {start} and {end}")
+
+
+def float_input(text: str, start: float = 0.0, end: float = 100_000_000.0) -> float:
+    '''
+    Get a valid float input from the user within a specified range.
+
+    Parameters:
+    - text (str): The prompt text for input.
+    - start (float): The minimum allowed value.
+    - end (float): The maximum allowed value.
+
+    Returns:
+    - float: The user input.
+    '''
+    while True:
+        inp = input(text)
+
+        if is_float(inp) and start <= float(inp) <= end:
+            return float(inp)
+        print(f"Wrong input, must be a number between {start} and {end}")
+
+
+def is_int(value: str) -> bool:
+    '''
+    Check if a given string can be converted to an integer.
+
+    Parameters:
+    - value (str): The input string.
+
+    Returns:
+    - bool: True if the string can be converted to an integer, False otherwise.
+    '''
+    try:
+        int(value)
+        return True
+    except ValueError:
+        return False
+
+
+def is_float(value: str) -> bool:
+    '''
+    Check if a given string can be converted to a float.
+
+    Parameters:
+    - value (str): The input string.
+
+    Returns:
+    - bool: True if the string can be converted to a float, False otherwise.
+    '''
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
