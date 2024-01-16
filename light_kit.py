96b5081f6c5cd79a7117db53b3e7b34a3d9da7d6
Code520455 <520455@eynesbury.sa.edu.au>
2024-01-16 11:13:29 +1030

    Update light_kit.py

--- light_kit.py
+++ light_kit.py
@@ -25,4 +25,27 @@ class LightCircuitKit(CircuitKit):
 
     Author: Rahul Saha
     '''
+
+    def __init__(self, name: str, count: int,
+                 light: Tuple[Light, int], switch: Tuple[Switch, int],
+                 battery: Tuple[Battery, int], wire: Tuple[Wire, int]):
+        super().__init__(name, count)
+        self._light: Tuple[Light, int] = light
+        self._switch: Tuple[Switch, int] = switch
+        self._wire = wire
+        self._battery: Tuple[Battery, int] = battery
+
+    @property
+    def price(self):
+        '''Get the total price of the circuit kit'''
+        total = 0.0
+        total += self._battery[0].price * self._battery[1]
+        total += self._light[0].price * self._light[1]
+        total += self._switch[0].price * self._switch[1]
+        total += self._wire[0].price * self._wire[1]
+        return total
     
+    @property
+    def component(self):
+        '''Get the doct of components in the circuit kit'''
+        return {self._battery[0]: self._battery[1], self._light[0]: self._light[1], self._switch[0]: self._switch[1], self._wire[0]: self._wire[1]}
