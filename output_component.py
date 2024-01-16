aa2fdb3554de356d362087bbcbf8cf81e762c083
Code520455 <520455@eynesbury.sa.edu.au>
2024-01-16 13:06:06 +1030

    Update output_component.py

--- output_component.py
+++ output_component.py
@@ -37,3 +37,55 @@ class OutputComponent(Component, ABC):
     @voltage.setter
     def voltage(self, value: float) -> None:
         '''Set the voltage of the output component'''
+        self._voltage = value
+
+    @property
+    def current(self) -> float:
+        '''Get the current of the output component'''
+        return self._current
+    
+    @current.setter
+    def current(self, value: float) -> None:
+        '''Set the current of the output component'''
+        self._current = value
+
+    @abstractmethod
+    def parse_csv(self, csv_tring: str) -> 'OutputComponent':
+        '''
+        Parse a CSV string to recreate an output component object.
+
+        Parameters:
+        - csv_string (str): The CSV string containing output component information.
+
+        Returns:
+        - OutputComponent: A new output component object.
+        '''
+        pass
+
+    @abstractmethod
+    def __eq__(self, __value: object) -> bool:
+        '''
+        Compare if two output component objects are equal.
+
+        Parameters:
+        - other (OutputComponent): The other output component to compare.
+
+        Returns:
+        - bool: True if the output components are equal, False otherwise.
+        '''
+        pass
+    
+    @abstractmethod
+    def display_user_friendly(self) -> str:
+        '''Display the output component in a user-friendly format.'''
+        pass
+
+    @abstractmethod
+    def display_csv(self) -> str:
+        '''Display the output component as a CSV string.'''
+        pass
+
+    def calculate_watt(self) -> float:
+        '''Calculate the wattage of the output component.'''
+        return self.voltage * self.current
+    
