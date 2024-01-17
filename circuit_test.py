5b6da136eecc8136b57c10615ccd59e3375cec49
Code520455 <130625646+Code520455@users.noreply.github.com>
2024-01-17 11:20:16 +1030

    Create circuit_test.py

---
+++ circuit_test.py
@@ -0,0 +1,31 @@
+# File: circuit_test.py
+# Author: RAHUL SAHA
+# ID: 520455
+# Email: 520455@learning.eynesbury.edu.au
+# Author: Rajeswari Roy
+# ID: 520456
+# Email: 520456@learning.eynesbury.edu.au
+# Description: circuit_test class
+# This is my own work as defined by the Academic Integrity Policy
+
+import unittest
+
+from light_kit import LightCircuitKit
+from sensor_kit import SensorCircuitKit
+from light_globe import LightGlobe
+from led_light import LEDLight
+from switch import Switch
+from wire import Wire
+from battery import Battery
+from power_supply import PowerSupply
+from sensor import Sensor
+from buzzer import Buzzer
+
+class CircuitTest(unittest.TestCase):
+    def test_light_kit(self):
+        '''
+        Test the light kit class
+        '''
+        light_info = (LightGlobe('Light Globe', 2.0, 2.2, 3.2, "warm"), 3)
+        battery_info = (Battery('Battery',"AA", 1.5, 3.1), 4)
+        switch_info = (Switch('Switch', 1.0, 1.0, 'push'), 2)
