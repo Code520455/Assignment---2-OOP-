1e2d013e9fd41e5316b7eb03ba2d8379639e1f38
Code520455 <130625580+RajeswariRoy1@users.noreply.github.com>
2024-01-20 11:22:11 +1030

    Update component_test.py

--- component_test.py
+++ component_test.py
@@ -2,10 +2,13 @@
 # Author: RAHUL SAHA
 # ID: 520455
 # Email: 520455@learning.eynesbury.edu.au
+# Author: Rajeswari Roy
+# ID: 520456
+# Email: 520456@learning.eynesbury.edu.au
 # Description: component_test
 # This is my own work as defined by the Academic Integrity Policy
 
-
+#This test is fully done by Rahul Saha
 import unittest
 
 from wire import Wire
